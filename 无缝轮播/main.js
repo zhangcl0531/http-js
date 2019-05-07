adf6782ad4bfe2322c27237e75d0d0c09e4d1ac5
zhangcl0531 <zclphone19940531@163.com>
2019-05-07 21:08:01 +0800

    无缝轮播2

--- 无缝轮播/main.js
+++ 无缝轮播/main.js
@@ -1,40 +1,96 @@
-setTimeout(function(){
-    $('.images > img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images > img:nth-child(2)').css({
-        transform: 'translateX(-100%)'
+// setTimeout(function(){
+//     $('.images > img:nth-child(1)').css({
+//         transform: 'translateX(-100%)'
+//     })
+//     $('.images > img:nth-child(2)').css({
+//         transform: 'translateX(-100%)'
+//     })
+
+//     $('.images > img:nth-child(1)').one('transitionend',function(){
+//         $(this).addClass('right').css({transform: 'none'})
+//     })
+// },2000)
+
+
+// setTimeout(function(){
+//     $('.images > img:nth-child(2)').css({
+//         transform: 'translateX(-200%)'
+//     })
+//     $('.images > img:nth-child(3)').css({
+//         transform: 'translateX(-100%)'
+//     })
+
+//     $('.images > img:nth-child(2)').one('transitionend',function(){
+//         $(this).addClass('right').css({transform: 'none'})
+//     })
+// },4000)
+
+
+// setTimeout(function(){
+//     $('.images > img:nth-child(3)').css({
+//         transform: 'translateX(-200%)'
+//     })
+//     $('.images > img:nth-child(1)').css({
+//         transform: 'translateX(-100%)'
+//     })
+
+//     $('.images > img:nth-child(3)').one('transitionend',function(){
+//         $(this).addClass('right').css({transform: 'none'})
+//     })
+// },6000)
+
+
+// $('.images > img:nth-child(1)').addClass('current')
+// $('.images > img:nth-child(2)').addClass('enter')
+// $('.images > img:nth-child(3)').addClass('enter')
+
+// setTimeout(function () {
+//     $('.images > img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend', function () {
+//         $(this).removeClass('leave').addClass('enter')
+//     })
+//     $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+// },2000)
+
+
+// setTimeout(function () {
+//     $('.images > img:nth-child(2)').removeClass('current').addClass('leave').one('transitionend', function () {
+//         $(this).removeClass('leave').addClass('enter')
+//     })
+//     $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
+// },4000)
+
+// setTimeout(function () {
+//     $('.images > img:nth-child(3)').removeClass('current').addClass('leave').one('transitionend', function () {
+//         $(this).removeClass('leave').addClass('enter')
+//     })
+//     $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
+// },6000)
+
+// setTimeout(function () {
+//     $('.images > img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend', function () {
+//         $(this).removeClass('leave').addClass('enter')
+//     })
+//     $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+// },8000)
+
+
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+
+let n = 1
+
+setInterval(function () {
+
+    $(`.images > img:nth-child(${n})`).removeClass('current').addClass('leave').one('transitionend', function () {
+        $(this).removeClass('leave').addClass('enter')
     })
 
-    $('.images > img:nth-child(1)').one('transitionend',function(){
-        $(this).addClass('right').css({transform: 'none'})
-    })
+    if(n===3){
+        n = 0
+    }
+
+    $(`.images > img:nth-child(${n+1})`).removeClass('enter').addClass('current')
+
+    n=n+1
 },2000)
-
-
-setTimeout(function(){
-    $('.images > img:nth-child(2)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images > img:nth-child(3)').css({
-        transform: 'translateX(-100%)'
-    })
-
-    $('.images > img:nth-child(2)').one('transitionend',function(){
-        $(this).addClass('right').css({transform: 'none'})
-    })
-},4000)
-
-
-setTimeout(function(){
-    $('.images > img:nth-child(3)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images > img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-
-    $('.images > img:nth-child(3)').one('transitionend',function(){
-        $(this).addClass('right').css({transform: 'none'})
-    })
-},6000)
