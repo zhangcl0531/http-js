2df5e3a16271de7a2a2be9f5257c1cd854213375
zhangcl0531 <zclphone19940531@163.com>
2019-05-22 17:12:27 +0800

    不刷新页面更新留言

--- resume_js/js/leancloud.js
+++ resume_js/js/leancloud.js
@@ -16,33 +16,39 @@ AV.init({
 // })
 
 let myForm = document.querySelector('#postmessage')
+let messagelist = document.querySelector('#messagelist')
 
 myForm.addEventListener('submit', function (e) {
     e.preventDefault()
     let content = myForm.querySelector('input[name=content]').value
+    let name = myForm.querySelector('input[name=name]').value
     var MessageObject = AV.Object.extend('message');
     var messageObject = new MessageObject();
     messageObject.save({
-        content: content
+        'content': content,
+        'name': name
     }).then(function (object) {
         alert('留言成功');
-        window.location.reload()
+        // window.location.reload()
         // document.querySelector('#messagelist').empty()
         // selectmessage()
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}:  ${object.attributes.content}`
+        messagelist.append(li)
     })
     myForm.querySelector('input[name=content]').value = ''
+    myForm.querySelector('input[name=name]').value = ''
 })
 
 
 function selectmessage() {
     var query = new AV.Query('message');
-    let messagelist = document.querySelector('#messagelist')
     query.find().then(function (messages) {
         console.log(messages)
         messages.forEach(function (message) {
             console.log(message.attributes.content)
             let li = document.createElement('li')
-            li.innerText = message.attributes.content
+            li.innerText = `${message.attributes.name}:  ${message.attributes.content}`
             messagelist.append(li)
         });
     }, function (error) {})
